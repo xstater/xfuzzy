1f194c56e5c48d8f31c92d91fad026548e605a76
xstater <a166224@163.com>
2023-05-24 00:44:17 +0800

    some more tries

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,87 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as rg from './ripgrep';
+import * as rg from 'ripgrep-wrapper';
+import { rgPath } from '@vscode/ripgrep';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('xfuzzy.searchLines', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage(rg.path);
+async function cmdSearchLines() {
+	const picker = vscode.window.createQuickPick();
+
+	picker.title = "Search Results:";
+	picker.canSelectMany = false;
+	picker.busy = true;
+
+	const tokenSource = new rg.CancellationTokenSource();
+
+	picker.onDidAccept(() => {
+		const selected = picker.selectedItems[0];
+		vscode.window.showInformationMessage(selected.label);
+		tokenSource.cancel();
+		picker.dispose();
 	});
+	picker.onDidHide(() => {
+		tokenSource.cancel();
+		picker.dispose();
+	});
+
+	picker.show();
+
+	const query: rg.ITextQuery = {
+		maxResults: 100000,
+		contentPattern: {
+			pattern: 'asddddd'
+		},
+		folderQueries: [
+			{ folder: '.' }
+		],
+	};
+
+	const searchEngine = new rg.TextSearchEngineAdapter(rgPath, query);
+	const result = await searchEngine.search(tokenSource.token, (res) => {
+		console.log('result:', res);
+
+		for (const r of res) {
+			r.results?.forEach(rr => {
+
+				function isITextSearchMatch(param: rg.ITextSearchResult): param is rg.ITextSearchMatch {
+					return !!(param as rg.ITextSearchMatch).preview;
+				}
+
+				if (isITextSearchMatch(rr)) {
+					picker.items =
+						picker.items.concat([{
+							label: rr.preview.text,
+							detail: rr.path
+						}]);
+				} else {
+					picker.items =
+						picker.items.concat([{
+							label: rr.text,
+							detail: rr.path
+						}]);
+				}
+			});
+		}
+
+	}, message => {
+		console.log('message', message);
+	});
+
+	console.log("asd", result);
+	if (picker.items.length === 0) {
+		vscode.window.showInformationMessage("No result can be found");
+		picker.hide();
+	}
+	if (result.limitHit) {
+		picker.title = picker.title + "(Results is up to 100000, stop searching)";
+	}
+	picker.busy = false;
+
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('xfuzzy.searchLines', cmdSearchLines);
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
