56f741f0560f9a6fe29716ea18b29b11ff0ecec7
xstater <a166224@163.com>
2023-05-31 10:23:51 +0800

    new picker

--- src/picker.ts
+++ src/picker.ts
@@ -1,56 +1,115 @@
 import * as vscode from 'vscode';
 
+export interface ToString {
+    toString(): string;
+}
 
-type Formatter<T> = (data: T) => string;
-
-class Separator<T> implements vscode.QuickPickItem {
+export class Separator<T extends ToString> implements vscode.QuickPickItem {
     label: string;
     private _data: T;
-    private _formatter: Formatter<T>;
 
     kind = vscode.QuickPickItemKind.Separator;
 
-    constructor(data: T, formatter: Formatter<T>) {
+    constructor(data: T) {
         this._data = data;
-        this._formatter = formatter;
-        this.label = formatter(data);
+        this.label = data.toString();
     }
 
 
     public set data(value: T) {
         this._data = value;
-        this.label = this._formatter(this._data);
+        this.label = this._data.toString();
     }
 
 
     public get data(): T {
         return this._data;
     }
-
-
-    public set formatter(newFormatter: Formatter<T>) {
-        this._formatter = newFormatter;
-        this.label = this._formatter(this._data);
-    }
-
-
-    public get formatter(): Formatter<T> {
-        return this._formatter;
-    }
 }
 
-interface CanBePickedItem extends vscode.QuickPickItem {
+export interface CanBePickedItem extends vscode.QuickPickItem {
     filePath: vscode.Uri;
     lineNumber: number;
     column: number;
 }
 
-async function jumpTo(item: CanBePickedItem) {
-    const doc = await vscode.workspace.openTextDocument(item.filePath);
-    const editor = await vscode.window.showTextDocument(doc);
 
-    const pos = new vscode.Position(item.lineNumber - 1, item.column - 1);
-    editor.selection = new vscode.Selection(pos, pos);
-    editor.revealRange(new vscode.Range(pos, pos), vscode.TextEditorRevealType.InCenter);
+export interface Results {
+    separator: Separator<ToString>,
+    items: CanBePickedItem[]
+}
+
+export class PickedItem {
+    filePath: vscode.Uri;
+    lineNumber: number;
+    column: number;
+
+    constructor(filePath: vscode.Uri, lineNumber: number, column: number) {
+        this.filePath = filePath;
+        this.lineNumber = lineNumber;
+        this.column = column;
+    }
+
+    async jumpTo() {
+        const doc = await vscode.workspace.openTextDocument(this.filePath);
+        const editor = await vscode.window.showTextDocument(doc);
+
+        const pos = new vscode.Position(this.lineNumber - 1, this.column - 1);
+        editor.selection = new vscode.Selection(pos, pos);
+        editor.revealRange(new vscode.Range(pos, pos), vscode.TextEditorRevealType.InCenter);
+    }
+
+}
+
+export class Picker {
+    rawPicker: vscode.QuickPick<CanBePickedItem | Separator<ToString>>;
+
+    private _count = 0;
+
+    constructor() {
+        this.rawPicker = vscode.window.createQuickPick();
+    }
+
+    public get count() {
+        return this._count;
+    }
+
+    dispose() {
+        this.rawPicker.dispose();
+    }
+
+    addResults(results: Results) {
+        const newResults =
+            ([results.separator] as (CanBePickedItem | Separator<ToString>)[])
+                .concat(results.items);
+        this._count += results.items.length;
+        this.rawPicker.items = this.rawPicker.items.concat(newResults);
+    }
+
+    clear() {
+        this.rawPicker.items = [];
+        this._count = 0;
+    }
+
+    pickOne(): Promise<PickedItem | undefined> {
+        this.rawPicker.show();
+
+        return new Promise((resolve, _) => {
+            this.rawPicker.onDidAccept(() => {
+                /// Can only be the PickedItem
+                /// [0] is safe, because DidAccept must have one element at least 
+                const selected = this.rawPicker.selectedItems[0] as CanBePickedItem;
+
+                resolve(new PickedItem(selected.filePath, selected.lineNumber, selected.column));
+
+                this.dispose();
+            });
+
+            this.rawPicker.onDidHide(() => {
+                resolve(undefined);
+                this.dispose();
+            });
+        });
+    }
 
 }
